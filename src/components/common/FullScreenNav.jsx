5d13cbc102789a76b7caa3a8a46cc0f3a4f167b1
TheGauravsahu <itsgauravsahu7@gmail.com>
2026-03-27 20:54:23 +0530

    feat : seo ready

--- src/components/common/FullScreenNav.jsx
+++ src/components/common/FullScreenNav.jsx
@@ -9,6 +9,7 @@ import { useContext } from "react";
 import { ApplicationContext } from "../../context/AppContext";
 import { Link } from "react-router-dom";
 import IndiaTime from "./IndiaTime";
+import logo_white from "../../assets/logo/white.png";
 
 const FullScreenNav = () => {
   const fullNavLinkRef = useRef(null);
@@ -77,18 +78,8 @@ const FullScreenNav = () => {
         >
           {/* top section */}
           <div className="navLink flex justify-between items-start p-2">
-            <div className="w-32 relative">
-              <svg
-                className="h-full w-full"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 103 44"
-              >
-                <path
-                  fill="white"
-                  fillRule="evenodd"
-                  d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"
-                ></path>
-              </svg>
+            <div className="text-white relative">
+             <h2 className="text-[7vw] font-[font-lausanne-300]">GS</h2>
             </div>
 
             <div
@@ -104,7 +95,10 @@ const FullScreenNav = () => {
             id="all-menu"
             className="flex items-center justify-center flex-col mt-[30vw] md:mt-0"
           >
-            <div onClick={()=>setIsNavbarOpen(false)} className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative">
+            <div
+              onClick={() => setIsNavbarOpen(false)}
+              className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative"
+            >
               <a
                 href="https://github.com/TheGauravsahu?tab=repositories"
                 target="_blank"
@@ -137,7 +131,10 @@ const FullScreenNav = () => {
               </a>
             </div>
 
-            <div onClick={()=>setIsNavbarOpen(false)} className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative">
+            <div
+              onClick={() => setIsNavbarOpen(false)}
+              className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative"
+            >
               <Link to="/about">
                 <h1 className="font-[font-lausanne-500] leading-[16vw] text-[16vw] md:text-[8vw] md:leading-[8vw] text-center">
                   ABOUT
@@ -167,7 +164,10 @@ const FullScreenNav = () => {
               </Link>
             </div>
 
-            <div onClick={()=>setIsNavbarOpen(false)} className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative">
+            <div
+              onClick={() => setIsNavbarOpen(false)}
+              className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative"
+            >
               <Link to="/gallary">
                 <h1 className="font-[font-lausanne-500]  leading-[16vw] text-[16vw] md:text-[8vw] md:leading-[8vw] text-center">
                   GALLARY
@@ -197,7 +197,10 @@ const FullScreenNav = () => {
               </Link>
             </div>
 
-            <div onClick={()=>setIsNavbarOpen(false)} className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative">
+            <div
+              onClick={() => setIsNavbarOpen(false)}
+              className="link origin-top border-t text-white w-full  border-gray-400 cursor-pointer relative"
+            >
               <a href="https://gauravblogs.vercel.app/" target="_blank">
                 <h1 className="font-[font-lausanne-500] leading-[16vw]  text-[16vw] md:text-[8vw] md:leading-[8vw] text-center">
                   BLOG
